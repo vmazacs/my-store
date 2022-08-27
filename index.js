2c380ecfcfd777af347ac18a8f8c3740cec387de
José Vidal Maza Alonso <vmaza@compusoluciones.com>
2022-08-27 16:36:27 -0500

    Creating routes directory. Creating products.router.js, users.router.js and categories.router.js files inside routes directory. Adding code to products.router.js, users.router.js and categories.router.js, cutting some routes code from index.js file in the root directory and pasting them in their respecitve routes file, this to add modularity and follow the Single Responsibility Principle.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const routerApi = require('./routes');
 
 const app = express();
 const port = 3001;
@@ -12,52 +12,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.product(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  res.send('Yo sou un filter');
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000,
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('No hay parámetros');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
+routerApi(app);
 
 app.listen(port, () => {
   console.log('Mi port ' + port);
