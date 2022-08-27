6faa222d34b40e7b6afe094d5ba1782f3f15f026
José Vidal Maza Alonso <vmaza@compusoluciones.com>
2022-08-27 15:55:52 -0500

    Creating file structure for node.js application. Creating package.json, .editorconfig, .eslintrc.json, .gitignore and index.js files. Installing dependencies for the app. Adding code to index.js file, importing express and @faker-js/faker, setting port, adding get routes to do some server tests with specific endpoints and dinamic endpoints (using faker to create random data to run tests).

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      name: faker.commerce.product(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+});
+
+app.get('/products/filter', (req, res) => {
+  res.send('Yo sou un filter');
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000,
+  });
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parámetros');
+  }
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
+app.listen(port, () => {
+  console.log('Mi port ' + port);
+});
