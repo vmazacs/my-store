edf38b2f83bcc0a2ad78832a2e5a0cf22d5e7e50
José Vidal Maza Alonso <vmaza@compusoluciones.com>
2022-08-28 15:00:12 -0500

    Adding code to index.js file setting middleware to parse json and only look requests where the Content-Type is json. Adding code to routes/index.js to set a super route for '/api/v1' and use it for the '/products', '/users' and '/categories' endpoints. Addng code to routes/products.router.js setting post, patch and delete routes, and handle status codes for the post '/' and get '/:id' routes.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3001;
 
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
--- routes/products.router.js
+++ routes/products.router.js
@@ -23,10 +23,42 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
+  if (id === '999') {
+    res.status(404).json({
+      message: 'not found'
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name: 'Product 2',
+      price: 2000,
+    });
+  }
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
   res.json({
+    message: 'created',
+    data: body,
     id,
-    name: 'Product 2',
-    price: 2000,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
   });
 });
 
