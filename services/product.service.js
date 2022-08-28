7fa41068ba27ac658faadab3ef03ea8b4e584a42
José Vidal Maza Alonso <vmaza@compusoluciones.com>
2022-08-28 16:27:18 -0500

    Modifying code in services/product.service.js file to all the ProductsService methods into asynchronous functions using async. For the find() method we implement new Promise for the return, with a resolve and reject parameters. Modifying code in routes/products.router.js file transforming all the callbacks for the routes into aynchronous functions, using async await syntax. Using try catch for the patch '/:id' route to handle errors correctly.

--- services/product.service.js
+++ services/product.service.js
@@ -19,7 +19,7 @@ class ProductsService {
     }
   }
 
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: faker.datatype.uuid(),
       ...data
@@ -28,15 +28,20 @@ class ProductsService {
     return newProduct;
   }
 
-  find() {
+  async find() {
+    return new Promise ((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    })
     return this.products;
   }
 
-  findOne(id) {
+  async findOne(id) {
     return this.products.find(item => item.id === id);
   }
 
-  update(id, changes) {
+  async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
       throw new Error('product not found');
@@ -49,7 +54,7 @@ class ProductsService {
     return this.products[index];
   }
 
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
       throw new Error('product not found');
