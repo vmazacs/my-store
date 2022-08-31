2a82ff1b906a6d976dee3f7b31727161fc60634f
José Vidal Maza Alonso <vmaza@compusoluciones.com>
2022-08-30 21:04:57 -0500

    Adding code to index.js file, fixing origin() method's if statement to check there is no origin specified (which means we are making requests to the api locally (from the same origin).

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('not allowed'));
